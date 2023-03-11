43916ac4baca567c2f1321f03542b63d22d20465
Yaroslava Terletska <yaroslava1408@ukr.net>
2023-03-11 22:08:35 +0100

    Add the buttons

--- js/script.js
+++ js/script.js
@@ -1,105 +1,95 @@
 "use strict";
 // Це коментар. Далі йде увесь код JS-файлу
-function getMoveName(){}
 
-let randomNumber = Math.floor(Math.random() * 3 + 1);
-console.log('The number is: ' + randomNumber);
-
-/*
-if(randomNumber == 1){
-  argComputerMove = 'rock';
-}
-else if (randomNumber == 2){
-  argComputerMove = 'paper';
-  }
-
-else if (randomNumber == 3){
-  argComputerMove = 'scissors';
-}  
-
-printMessage('My move is: ' + argComputerMove);*/
-
-let argComputerMove = getMoveName(randomNumber);
-
-function getMoveName (argComputerMove) {
-if (argComputerMove  == 1){
-  return 'rock';
-}
-else if (argComputerMove  == 2){
-  return 'paper';
-  }
-
-else if (argComputerMove  == 3){
-  return'scissors';
-} 
-} 
-getMoveName (argComputerMove);
-printMessage('My move is: ' + argComputerMove);
-
-
-let playerInput = prompt('Choose your move! 1: rock, 2: paper, 3: scissors.');
-console.log('The player chose: ' + playerInput);
-
-let argPlayerMove = getMoveName(playerInput);
-printMessage('Your move is: ' + argPlayerMove);
-
-
-/*if(playerInput == '1'){
+function playGame(playerInput){  
   
-  argPlayerMove = 'rock';
-}
+    function getMoveName (argMoveId) {
+    if (argMoveId  == 1){
+    return 'rock';
+    }
+    else if (argMoveId  == 2){
+    return 'paper';
+    }
+    else if (argMoveId  == 3){
+    return'scissors';
+    } 
+  } 
+  
+  clearMessages();
 
-else if (playerInput == '2'){
-    
-  argPlayerMove = 'paper';
-  }
-else if (playerInput == '3'){
-  argPlayerMove = 'scissors';
-  }*/
+  let randomNumber = Math.floor(Math.random() * 3 + 1);
+  console.log('The number is: ' + randomNumber);
+
+  let argComputerMove = getMoveName (randomNumber);
+  printMessage('My move is: ' + argComputerMove);
+ 
+ let argPlayerMove = getMoveName(playerInput);
+  printMessage('Your move is: ' + argPlayerMove); 
+  
+
 
 function displayResult (argComputerMove, argPlayerMove) {
   console.log('moves:', argComputerMove, argPlayerMove);
   printMessage('I choose ' + argComputerMove + ', and you ' + argPlayerMove);
 
-if(argComputerMove == 'rock' && argPlayerMove == 'paper'){
+    if(argComputerMove == 'rock' && argPlayerMove == 'paper'){
+    printMessage ('You win!');
+    }
+    else if(argComputerMove == 'paper' && argPlayerMove == 'scissors'){
     printMessage('You win!');
-  }
-else if(argComputerMove == 'paper' && argPlayerMove == 'scissors'){
+    }
+    else if(argComputerMove == 'scissors' && argPlayerMove == 'rock'){
     printMessage('You win!');
-  }
-else if(argComputerMove == 'scissors' && argPlayerMove == 'rock'){
-    printMessage('You win!');
-  }
-else if(argComputerMove == 'paper' && argPlayerMove == 'rock'){
+    }
+    else if(argComputerMove == 'paper' && argPlayerMove == 'rock'){
     printMessage('You lose!');
-  }
-else if(argComputerMove == 'scissors' && argPlayerMove == 'paper'){
+    }
+    else if(argComputerMove == 'scissors' && argPlayerMove == 'paper'){
     printMessage('You lose!');
-  }
-else if(argComputerMove == 'rock' && argPlayerMove == 'scissors'){
+    }
+    else if(argComputerMove == 'rock' && argPlayerMove == 'scissors'){
     printMessage('You lose!');
-  }
-else if(argComputerMove == 'rock' && argPlayerMove == 'rock'){
+    }
+    else if(argComputerMove == 'rock' && argPlayerMove == 'rock'){
     printMessage('A draw!');
-}
-else if(argComputerMove == 'paper' && argPlayerMove == 'paper'){
+    }
+    else if(argComputerMove == 'paper' && argPlayerMove == 'paper'){
     printMessage('A draw!');
-}
-else if(argComputerMove == 'scissors' && argPlayerMove == 'scissors'){
+    }
+    else if(argComputerMove == 'scissors' && argPlayerMove == 'scissors'){
     printMessage('A draw!');
-}
-
-else if(playerInput >3){    
+    }
+    else if(playerInput >3){    
     printMessage('Please, use number from 1 to 3');
-}
-
-else if(playerInput <1){    
+    }
+    else if(playerInput <1){    
     printMessage('Please, use number from 1 to 3');
-}
-}
+    }
+  }
+
 displayResult (argComputerMove, argPlayerMove);
+console.log (argComputerMove  + ":" + argPlayerMove);  
+}
 
-console.log (getMoveName ('2'));
+document.getElementById('play-rock').addEventListener('click', function(){
+  playGame(1);
+});
+document.getElementById('play-paper').addEventListener('click', function(){
+  playGame(2);
+});
+document.getElementById('play-scissors').addEventListener('click', function(){
+  playGame(3);
+}); 
+
+
+
+
+
+
+
+
+
+
 
 
 
